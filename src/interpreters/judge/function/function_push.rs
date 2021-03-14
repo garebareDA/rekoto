2968cee591bf725754aee684541e08eeb40d443f
garebareDA <garedaiki@gmail.com>
2021-03-14 16:11:03 +0000

    add:function_run

--- src/interpreters/judge/function/function_push.rs
+++ src/interpreters/judge/function/function_push.rs
@@ -1,11 +1,216 @@
 use super::super::super::interpreter::Interpreter;
 use crate::error::result;
 use crate::parser::ast::ast;
-use crate::parser::ast::ast::{Node, Syntax};
+use crate::parser::ast::ast::{Node, Syntax, Type, Types};
 
 impl Interpreter {
-  pub(crate) fn function_run(&mut self, fun: &ast::FunctionAST, call:&ast::CallAST) {
-    
+  pub(crate) fn function_run(
+    &mut self,
+    fun: &ast::FunctionAST,
+    call: &ast::CallAST,
+  ) -> Result<(), result::Error> {
+    let argments = call.get_argment();
+    let params = fun.get_param();
+
+    if argments.len() != params.len() {
+      return Err(result::Error::InterpreterError(format!(
+        "function argments error {} {} paramas",
+        call.get_name(),
+        params.len()
+      )));
+    }
+
+    for (index, param) in params.iter().enumerate() {
+      let types: &ast::Types;
+      let name: &str;
+      match param {
+        Syntax::Var(var) => match var.get_type() {
+          Some(t) => {
+            types = t;
+            name = var.get_name();
+          }
+          None => {
+            return Err(result::Error::InterpreterError(format!(
+              "function {} argment {} th not found type impossible interpreter bug",
+              call.get_name(),
+              index,
+            )));
+          }
+        },
+        _ => {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th not found type impossible interpreter bug",
+            call.get_name(),
+            index,
+          )));
+        }
+      }
+
+      match argments.get(index) {
+        Some(args) => match self.function_type_check(args, types, index, name, call) {
+          Ok(()) => {}
+
+          Err(e) => {
+            return Err(e);
+          }
+        },
+
+        None => {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th not found impossible interpreter bug",
+            call.get_name(),
+            index,
+          )));
+        }
+      }
+    }
+
+    return Ok(());
+  }
+
+  fn function_type_check(
+    &mut self,
+    args: &Syntax,
+    types: &Types,
+    index: usize,
+    name: &str,
+    call: &ast::CallAST,
+  ) -> Result<(), result::Error> {
+    match args {
+      Syntax::Bool(_) => {
+        if types != &Types::Bool {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th miss matched type",
+            call.get_name(),
+            index,
+          )));
+        }
+
+        let mut var = ast::VariableAST::new(name, false, true);
+        var.push_node(args.clone());
+        match self.push_var(&var) {
+          Ok(()) => {}
+          Err(e) => {
+            return Err(e);
+          }
+        }
+      }
+      Syntax::Str(_) => {
+        if types != &Types::String {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th miss matched type",
+            call.get_name(),
+            index,
+          )));
+        }
+
+        let mut var = ast::VariableAST::new(name, false, true);
+        var.push_node(args.clone());
+        match self.push_var(&var) {
+          Ok(()) => {}
+          Err(e) => {
+            return Err(e);
+          }
+        }
+      }
+      Syntax::Num(_) => {
+        if types != &Types::Number {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th miss matched type",
+            call.get_name(),
+            index,
+          )));
+        }
+
+        let mut var = ast::VariableAST::new(name, false, true);
+        var.push_node(args.clone());
+        match self.push_var(&var) {
+          Ok(()) => {}
+          Err(e) => {
+            return Err(e);
+          }
+        }
+      }
+
+      //TODO serch_verでTypesを返すようにする
+      Syntax::Var(var) => match self.serch_var(var.get_name()) {
+        Some(vars) => match vars {
+          Syntax::Bool(_) => {
+            if types != &Types::Bool {
+              return Err(result::Error::InterpreterError(format!(
+                "function {} argment {} th miss matched type",
+                call.get_name(),
+                index,
+              )));
+            }
+
+            let mut var = ast::VariableAST::new(name, false, true);
+            var.push_node(vars.clone());
+            match self.push_var(&var) {
+              Ok(()) => {}
+              Err(e) => {
+                return Err(e);
+              }
+            }
+          }
+
+          Syntax::Str(_) => {
+            if types != &Types::String {
+              return Err(result::Error::InterpreterError(format!(
+                "function {} argment {} th miss matched type",
+                call.get_name(),
+                index,
+              )));
+            }
+            let mut var = ast::VariableAST::new(name, false, true);
+            var.push_node(args.clone());
+            match self.push_var(&var) {
+              Ok(()) => {}
+              Err(e) => {
+                return Err(e);
+              }
+            }
+          }
+
+          Syntax::Num(_) => {
+            if types != &Types::Number {
+              return Err(result::Error::InterpreterError(format!(
+                "function {} argment {} th miss matched type",
+                call.get_name(),
+                index,
+              )));
+            }
+            let mut var = ast::VariableAST::new(name, false, true);
+            var.push_node(args.clone());
+            match self.push_var(&var) {
+              Ok(()) => {}
+              Err(e) => {
+                return Err(e);
+              }
+            }
+          }
+
+          _ => {}
+        },
+        None => {
+          return Err(result::Error::InterpreterError(format!(
+            "function {} argment {} th miss matched type",
+            call.get_name(),
+            index,
+          )));
+        }
+      },
+
+      _ => {
+        return Err(result::Error::InterpreterError(format!(
+          "function {} argment {} th not found type impossible interpreter bug",
+          call.get_name(),
+          index,
+        )));
+      }
+    }
+
+    return Ok(());
   }
 
   pub(crate) fn function_init(&mut self, root: &ast::RootAST) -> Result<(), result::Error> {
