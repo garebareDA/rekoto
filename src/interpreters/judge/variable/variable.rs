742710f77d56fbb83ae6c423d8d6234304045bde
garebareDA <garedaiki@gmail.com>
2021-03-19 15:16:41 +0000

    add:inc dec oplator

--- src/interpreters/judge/variable/variable.rs
+++ src/interpreters/judge/variable/variable.rs
@@ -1,6 +1,9 @@
 use super::super::super::interpreter::Interpreter;
 use crate::error::result;
+use crate::lexer::token;
+use crate::parser::ast::ast;
 use crate::parser::ast::ast::{Node, Syntax, VariableAST};
+static TOKEN: token::Token = token::Token::new();
 
 impl Interpreter {
   pub(crate) fn variable(&mut self, var: &VariableAST) -> Result<(), result::Error> {
@@ -40,6 +43,50 @@ impl Interpreter {
 
           Syntax::Num(_) => {
             return self.is_node_index(vars, var);
+          }
+
+          Syntax::Bin(bin) => {
+            let serch = self.serch_var(vars.get_name());
+            match serch.0 {
+              Some(serched) => match serched {
+                Syntax::Num(num) => {
+                  let mut result = num.get_num();
+                  let mut var_ast = ast::VariableAST::new(
+                    vars.get_name(),
+                    true,
+                    false,
+                  );
+
+                  if TOKEN._inc == bin.get_token() {
+                    result = result + 1;
+                    var_ast.push_node(Syntax::Num(ast::NumberAST::new(result)));
+                    self.push_var(&var_ast)?;
+                    return Ok(());
+                  }
+
+                  if TOKEN._dec == bin.get_token() {
+                    result = result - 1;
+                    var_ast.push_node(Syntax::Num(ast::NumberAST::new(result)));
+                    self.push_var(&var_ast)?;
+                    return Ok(());
+                  }
+
+                  return Err(result::Error::InterpreterError(format!("{} only increment and decrement operators are supported.", vars.get_name())));
+                }
+                _ => {
+                  return Err(result::Error::InterpreterError(format!(
+                    "{} is missmatched type",
+                    vars.get_name()
+                  )));
+                }
+              },
+              None => {
+                return Err(result::Error::InterpreterError(format!(
+                  "{} variable not initialized",
+                  vars.get_name()
+                )));
+              }
+            }
           }
 
           _ => {
